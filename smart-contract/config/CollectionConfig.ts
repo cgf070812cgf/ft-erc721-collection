5839f03c737337ca1a5a27d0f147c9afa642e577
cgf070812cgf <122876782@qq.com>
2022-08-01 14:35:25 +0800

    first commit

---
+++ smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,36 @@
+import CollectionConfigInterface from "../lib/CollectionConfigInterface";
+import * as Networks from "../lib/Networks";
+import * as Marketplaces from "../lib/Marketplaces";
+import whitelistAddresses from "./whitelist.json";
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: Networks.hardhatLocal,
+  mainnet: Networks.ethereumMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: "Cooleye",
+  tokenName: "My NFT Token",
+  tokenSymbol: "MNT",
+  hiddenMetadataUri:
+    "ipfs://QmRRX3N97gQE6Cacr8N1TtYVTawqyVGcYhKy8swfybWucr/hidden.json",
+  maxSupply: 10000,
+  whitelistSale: {
+    price: 0.05,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.07,
+    maxMintAmountPerTx: 2,
+  },
+  publicSale: {
+    price: 0.09,
+    maxMintAmountPerTx: 5,
+  },
+  contractAddress: "0xF3B8816002225186264Fd394611FFAc9d7472CD4",
+  marketplaceIdentifier: "my-nft-token",
+  marketplaceConfig: Marketplaces.openSea,
+  whitelistAddresses,
+};
+
+export default CollectionConfig;
